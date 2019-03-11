4dd419cd6cb6dd0b659cc74a12df37e715651e18
nawatend <32384185+nawatend@users.noreply.github.com>
2019-03-11 19:07:57 +0100

    final touch

---
+++ tinder/README.md
@@ -0,0 +1,5 @@
+##Tinder
+[x] Inladen van Random Users via de RandomUserMe API
+[x] Toon info van de gebruiker op de LED-matrix
+[x] Like / Dislike de actieve gebruker
+[x] Hou alle data bij in een extern bestand data.json
--- tinder/index.py
+++ tinder/index.py
@@ -1,75 +1,79 @@
 from sense_hat import SenseHat
-##for api and data
+# for api and data
 import requests
 import json
 import sys
 
 sense = SenseHat()
 
-#var 
+# var
 profileName = ""
 profileAge = 0
-profileLocation = "" 
+profileLocation = ""
+
 
 def loadOldData():
     with open('data.json') as json_data:
-        oldData = json.load(json_data)
-        return oldData
-    
+    oldData = json.load(json_data)
+    return oldData
+
+
 def likeProfile():
     global profileName
     global profileAge
     global profileLocation
-    
-    newProfile = {'name': profileName,'age':profileAge,'location':profileLocation, 'rate': 'like'}
-    
-    #update data.json
+
+    newProfile = {'name': profileName, 'age': profileAge,
+                  'location': profileLocation, 'rate': 'like'}
+
+    # update data.json
     with open('data.json', "a") as json_file:
         json_file.write("{}\n".format(json.dumps(newProfile)))
-    
-    
+
+
 def dislikeProfile():
     global profileName
     global profileAge
     global profileLocation
-    
-    newProfile = {'name': profileName,'age':profileAge,'location':profileLocation, 'rate': 'dislike'}
-    
-    #update data.json
+
+    newProfile = {'name': profileName, 'age': profileAge,
+                  'location': profileLocation, 'rate': 'dislike'}
+
+    # update data.json
     with open('data.json', "a") as json_file:
         json_file.write("{}\n".format(json.dumps(newProfile)))
-  
+
+
 def getProfile():
     global profileName
     global profileAge
     global profileLocation
-    
+
     profileData = requests.get("https://randomuser.me/api/").json()
     profileName = profileData['results'][0]['name']['first']
     profileAge = profileData['results'][0]['dob']['age']
     profileLocation = profileData['results'][0]['location']['city']
-    
-    
+
     sense.stick.direction_right = likeProfile
     sense.stick.direction_left = dislikeProfile
-    
-    
+
+
 def printProfileInfo():
     global profileName
     global profileAge
     global profileLocation
-    #start color green of per profile
-    c_text = (0,200,25)
-    sense.show_message(profileName,text_colour=c_text)
+    # start color green of per profile
+    c_text = (0, 200, 25)
+    sense.show_message(profileName, text_colour=c_text)
     sense.show_message(str(profileAge))
     sense.show_message(profileLocation)
 
+
 try:
     while True:
-      getProfile()
-      printProfileInfo()
+    getProfile()
+    printProfileInfo()
 except (KeyboardInterrupt, SystemExit):
     print('Exit Program')
     sense.clear()
     sys.exit(0)
-
