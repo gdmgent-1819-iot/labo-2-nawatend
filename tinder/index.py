c5b45312234613bfeb9c55c0da588cb55a6249c0
Nawang <32384185+nawatend@users.noreply.github.com>
2019-03-11 19:02:09 +0100

    Update index.py

--- tinder/index.py
+++ tinder/index.py
@@ -1,20 +1,3 @@
-##imports
-from sense_hat import SenseHat
-##for api and data
-import requests
-import json
-
-sense = SenseHat()
-
-
-def getProfile():
-    profileData = requests.get('https://randomuser.me/api').json()
-
-    global profileName
-    global profileAge
-    global profileLocation
-
-    profileName = profileData['results'][0]['name']['first'];##imports
 from sense_hat import SenseHat
 ##for api and data
 import requests
@@ -27,26 +10,66 @@ sense = SenseHat()
 profileName = ""
 profileAge = 0
 profileLocation = "" 
+
+def loadOldData():
+    with open('data.json') as json_data:
+        oldData = json.load(json_data)
+        return oldData
+    
+def likeProfile():
+    global profileName
+    global profileAge
+    global profileLocation
+    
+    newProfile = {'name': profileName,'age':profileAge,'location':profileLocation, 'rate': 'like'}
+    
+    #update data.json
+    with open('data.json', "a") as json_file:
+        json_file.write("{}\n".format(json.dumps(newProfile)))
+    
+    
+def dislikeProfile():
+    global profileName
+    global profileAge
+    global profileLocation
+    
+    newProfile = {'name': profileName,'age':profileAge,'location':profileLocation, 'rate': 'dislike'}
+    
+    #update data.json
+    with open('data.json', "a") as json_file:
+        json_file.write("{}\n".format(json.dumps(newProfile)))
   
 def getProfile():
     global profileName
     global profileAge
     global profileLocation
+    
     profileData = requests.get("https://randomuser.me/api/").json()
-    profileName = profileData.results[0].name.first + profileData.results[0].name.last
-    profileAge = profileData['results'][0]['gender']
+    profileName = profileData['results'][0]['name']['first']
+    profileAge = profileData['results'][0]['dob']['age']
     profileLocation = profileData['results'][0]['location']['city']
-    print("globally:", profileName)
-
+    
+    
+    sense.stick.direction_right = likeProfile
+    sense.stick.direction_left = dislikeProfile
+    
+    
 def printProfileInfo():
     global profileName
     global profileAge
     global profileLocation
-  
-    sense.show_message(profileName)
-    sense.show_message(profileAge)
+    #start color green of per profile
+    c_text = (0,200,25)
+    sense.show_message(profileName,text_colour=c_text)
+    sense.show_message(str(profileAge))
     sense.show_message(profileLocation)
 
-getProfile()
-# sense.stick.direction_left = like
-# sense.stick.direction_right = dislike
+try:
+    while True:
+      getProfile()
+      printProfileInfo()
+except (KeyboardInterrupt, SystemExit):
+    print('Exit Program')
+    sense.clear()
+    sys.exit(0)
+
